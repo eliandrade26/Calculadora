5f3317349116a81a20803d363f74aab41980b40b
eliandrade26 <eli_carlos_14@hotmail.com>
2024-03-13 22:20:47 -0300

    Modificação

--- script.js
+++ script.js
@@ -1,59 +1,8 @@
-const one = document.getElementById("one");
-one.addEventListener('click', function () {
-    console.log('1');
-});
+const keynumbers = array.from(document.querySelectorAll("numbers"));
+const display = document.querySelector("display");
+const keyOperators = array.from(querySelectorAll("Operations"));
 
-const two = document.getElementById("two");
-two.addEventListener('click', function () {
-    console.log('2');
-});
-
-const three = document.getElementById("three");
-three.addEventListener('click', function () {
-    console.log('3');
-});
-
-const four = document.getElementById("four");
-four.addEventListener('click', function () {
-    console.log('4');
-});
-
-const five = document.getElementById("five");
-five.addEventListener('click',function() {
-    console.log('5');
-});
-
-const six = document.getElementById("six");
-six.addEventListener('click', function () {
-    console.log('6');
-});
-
-const seven = document.getElementById("seven");
-seven.addEventListener('click', function () {
-    console.log('7');
-});
-
-const eight = document.getElementById("eight");
-eight.addEventListener('click', function () {
-    console.log(8);
-});
-
-const nine = document.getElementById('nine');
-nine.addEventListener('click', function () {
-    console.log("9");
-});
-
-const zero = document.getElementById("zero");
-zero.addEventListener('click',function(){
-    console.log("0");
-})
-
-const tela=document.getElementById(".tela");
-
-tela.addEventListener('click',function(){
-
-    const soma =one.value;
-
-    display.value =+ soma;
-
+keynumbers.array.forEach(keynumbers => {
+    keynumbers.addEventListener ('click',insertnumber)
+    
 });
